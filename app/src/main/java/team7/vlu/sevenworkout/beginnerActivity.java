477ebb9b9a1c9b9abbad20406a8ad0796d3bc98c
Phong-fashonality <Ho_Boong28@outlook.com.vn>
2021-11-24 15:54:38 +0700

    phong code

--- app/src/main/java/team7/vlu/sevenworkout/beginnerActivity.java
+++ app/src/main/java/team7/vlu/sevenworkout/beginnerActivity.java
@@ -3,6 +3,9 @@ package team7.vlu.sevenworkout;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
 import android.content.Intent;
@@ -18,71 +21,58 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import team7.vlu.sevenworkout.phong_interface.IClickItemUserListener;
 
 public class beginnerActivity extends AppCompatActivity {
-    int alo = 5;
-    ListView listView;
-        String mTitleB[] = {"Toe Touch", "High Knees", "Hand & Foot Touch", "Jumping Jack", "Stationary Lunge", "Burpee", "Side Lunge Touch", "Step Ups"};
-    String mDescriptionB[] = {"20s", "20s", "15s", "30s", "25s", "20s", "25s", "30s"};
-    int mImgageB [] ={R.drawable.ex_5, R.drawable.ex_6, R.drawable.ex_11, R.drawable.ex_12, R.drawable.ex_22,R.drawable.ex_24,R.drawable.ex_25, R.drawable.ex_27};
+
+    private RecyclerView rcvData;
+    private UserAdapter userAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_beginner);
 
-        listView = findViewById(R.id.list_pratice);
+        rcvData = findViewById(R.id.rcv_data);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        rcvData.setLayoutManager(linearLayoutManager);
 
-        //create Adapter
-        MyAdapter adapter = new MyAdapter(this, mTitleB, mDescriptionB, mImgageB);
-        listView.setAdapter(adapter);
+        RecyclerView.ItemDecoration itemDecoration = new DividerItemDecoration(this , DividerItemDecoration.VERTICAL);
+        rcvData.addItemDecoration(itemDecoration);
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        userAdapter = new UserAdapter(getListUser(), new IClickItemUserListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                if (position ==  0) {
-                    Toast.makeText(beginnerActivity.this, "Twitter Description", Toast.LENGTH_SHORT).show();
-                }
-                if (position ==  1) {
-                    Toast.makeText(beginnerActivity.this, "Twitter Description", Toast.LENGTH_SHORT).show();
-                }
-                if (position ==  2) {
-                    Toast.makeText(beginnerActivity.this, "Twitter Description", Toast.LENGTH_SHORT).show();
-                }
-                if (position ==  3) {
-                    Toast.makeText(beginnerActivity.this, "Instagram Description", Toast.LENGTH_SHORT).show();
-                }
-                if (position ==  4) {
-                    Toast.makeText(beginnerActivity.this, "Youtube Description", Toast.LENGTH_SHORT).show();
-                }
+            public void onClickItemUser(User user) {
+                onClickGoToDetail(user);
             }
         });
+        rcvData.setAdapter(userAdapter);
 
     }
 
-    class MyAdapter extends ArrayAdapter<String> {
-
-        Context context;
-        String rTitleB[];
-        String rDesB[];
-        int rImage[];
-
-        public MyAdapter(Context c, String TitleB[], String Description[], int ImgageB[]) {
-            super(c, R.layout.item_practice, R.id.textView1, TitleB);
-            this.context = c;
-            this.rTitleB = TitleB;
-            this.rDesB = Description;
-            this.rImage = ImgageB;
-        }
-
-        @NonNull
-        @Override
-        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-            LayoutInflater layoutInflater =(LayoutInflater)getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            View view = layoutInflater.inflate(R.layout.item_practice, parent, false);
-            ImageView images = view.findViewById(R.id.imgBeginnerList);
-            TextView myTitle = view.findViewById(R.id.textView1);
-            TextView myDescription = view.findViewById(R.id.textView2);
-            return view;
-        }
+    private List<User> getListUser() {
+        List<User> list = new ArrayList<>();
+        list.add(new User(R.drawable.ex_5, "Toe Touch", "30s"));
+        list.add(new User(R.drawable.ex_6, "High Knees", "30s"));
+        list.add(new User(R.drawable.ex_11, "Hand & Foot Touch", "30s"));
+        list.add(new User(R.drawable.ex_12, "Jumping Jack", "30s"));
+        list.add(new User(R.drawable.ex_22, "Stationary Lunge", "30s"));
+        list.add(new User(R.drawable.ex_24, "Burpee", "30s"));
+        list.add(new User(R.drawable.ex_25, "Side Lunge Touch", "30s"));
+        list.add(new User(R.drawable.ex_27, "Step Ups", "30s"));
+        return  list;
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+    private void onClickGoToDetail(User user) {
+        Intent intent = new Intent(this, DetailActivity.class);
+        Bundle bundle = new Bundle();
+        bundle.putSerializable("object_user", user);
+        intent.putExtras(bundle);
+        startActivity(intent);
     }
 }
