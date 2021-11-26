147f1df577d5278c1fb4469a6456fe3ce90dd2d2
thinh <thinhle642@gmail.com>
2021-11-26 21:09:54 +0700

    Create About, pushnotifycation!

--- app/src/main/java/team7/vlu/sevenworkout/PushNotifycationService.java
+++ app/src/main/java/team7/vlu/sevenworkout/PushNotifycationService.java
@@ -1,4 +1,38 @@
 package team7.vlu.sevenworkout;
 
-public class PushNotifycationService {
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationManagerCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class PushNotifycationService extends FirebaseMessagingService {
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+
+        remoteMessage.getNotification().getTitle();
+        String title = remoteMessage.getNotification().getTitle();
+        String text = remoteMessage.getNotification().getBody();
+        final String CHANEL_ID = "HEADS_UP_NOTIFICATION";
+        NotificationChannel channel = new NotificationChannel(
+                CHANEL_ID,
+                "Heads Up Notification",
+                NotificationManager.IMPORTANCE_HIGH
+        );
+
+        getSystemService(NotificationManager.class).createNotificationChannel(channel);
+        Notification.Builder notification = new Notification.Builder(this, CHANEL_ID)
+                .setContentTitle(title)
+                .setContentText(text)
+                .setSmallIcon(R.drawable.ic_fintness)
+                .setAutoCancel(true);
+
+        NotificationManagerCompat.from(this).notify(1, notification.build());
+
+        super.onMessageReceived(remoteMessage);
+    }
 }
