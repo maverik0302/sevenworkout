4f310b05dc1ada9c427703f5355745ee92035bc0
thinh <thinhle642@gmail.com>
2021-11-30 14:22:43 +0700

    CountDown

--- app/src/main/java/team7/vlu/sevenworkout/DetailActivity.java
+++ app/src/main/java/team7/vlu/sevenworkout/DetailActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import pl.droidsonroids.gif.GifImageView;
+
 public class DetailActivity extends AppCompatActivity {
 
     @Override
@@ -16,8 +18,13 @@ public class DetailActivity extends AppCompatActivity {
         if(bundle == null){
             return;
         }
+
         Exercise exercise = (Exercise) bundle.get("object_user");
         TextView tvNameUser = findViewById(R.id.tv_name_user);
+        tvNameUser.setText(exercise.getName());
+
+        GifImageView gifImageView = findViewById(R.id.img_gif);
+        gifImageView.setImageResource(exercise.getResourceId());
         tvNameUser.setText(exercise.getName());
     }
 }
